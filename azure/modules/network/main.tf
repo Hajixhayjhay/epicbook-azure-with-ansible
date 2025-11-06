d11c7bb34e07fe1c834c9c97be959804623b66b8
hajixhayjhay <akandehajarat@gmail.com>
2025-11-06 11:11:01 +0800

    updated network module

--- azure/modules/network/main.tf
+++ azure/modules/network/main.tf
@@ -26,6 +26,17 @@ resource "azurerm_subnet" "private_subnet" {
   resource_group_name  = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.2.0/24"]
+
+delegation {
+    name = "mysql-flexible-server-delegation"
+
+    service_delegation {
+      name    = "Microsoft.DBforMySQL/flexibleServers"
+      actions = [
+        "Microsoft.Network/virtualNetworks/subnets/action"
+      ]
+    }
+  }
 }
 
 # -------------------------------
