80187541029a8b5ba819b76fc3b13c7764372544
hajixhayjhay <akandehajarat@gmail.com>
2025-11-12 09:43:14 +0800

    updated files

--- azure/provider.tf
+++ azure/provider.tf
@@ -1,4 +1,4 @@
 provider "azurerm" {
   features {}
-  subscription_id = "a1f0a6b7-42db-4e30-a094-dec5bc05aa08"
+  subscription_id = var.subscription_id
 }
--- azure/variables.tf
+++ azure/variables.tf
@@ -71,3 +71,7 @@ variable "ssh_public_key" {
   type        = string
 }
 
+variable "subscription_id" {
+  description = "Azure subscription ID"
+  type        = string
+}
